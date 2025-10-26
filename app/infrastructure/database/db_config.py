2f0b0057798f1707d34daed7982f2b91a1a83d09
gabrielbaute <gabrielbaute@gmail.com>
2025-10-26 17:11:21 -0400

    Tipado explícito en funcin de inicializacion de db y docstring

--- app/infrastructure/database/db_config.py
+++ app/infrastructure/database/db_config.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
 
-def init_db(app):
-    db.init_app(app)
+def init_db(application: Flask):
+    """Inicializa la base de datos
+    
+    Args:
+        app (Flask): Instancia de la aplicación Flask
+    """
+    db.init_app(application)
